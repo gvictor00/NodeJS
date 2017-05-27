2b79b57fdb1a7233e054249a9f43a173343bc46e
George <gvictor00@gmail.com>
2017-05-27 01:36:29 -0300

    InstalaçãoNodemoon

--- app.js
+++ app.js
@@ -7,27 +7,18 @@ var app = express();
 //Notifica ao Express que vamos usar o EJS para gerar as views
 app.set('view engine','ejs');
 
-app.get('/tecnologia', function(req, res){
+app.get('/', function(req, res){
 	//Forma de chamada do bloco sobre tecnologia do EJS
 	//O Render busca as informações e arquivos .ejs no caminho especificado.
-	res.render("secao/tecnologia");
+	res.render("home/index");
 });
 
-app.get('/', function(req, res){
-	//Envia o HTML. A função SEND é caracteristica do EXPRESS, enquanto end é do Node
-	res.send("<html><body>Portal de Noticias</body></html>")
+app.get('/fomulario_inclusao_noticia', function(req, res){
+	res.render("admin/form_add_noticia");
 });
 
-//app.get('/tecnologia', function(req, res){
-	//res.send("<html><body>Portal de Noticias Tecnologia</body></html>")
-//});
-
-app.get('/moda', function(req, res){
-	res.send("<html><body>Portal de Noticias de Moda</body></html>")
-});
-
-app.get('/beleza', function(req, res){
-	res.send("<html><body>Portal de Noticias de Beleza</body></html>")
+app.get('/noticias', function(req, res){
+	res.render("noticias/noticias");
 });
 
 //Escuta a porta #, substituindo o createServer da biblioteca HTTP

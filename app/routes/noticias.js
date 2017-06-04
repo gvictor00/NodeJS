ecd4566d98bd86f82f346be55273d55bef67cc50
George <gvictor00@gmail.com>
2017-06-03 22:20:22 -0300

    Alteração app/routes/noticias.js

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -9,6 +9,14 @@ module.exports = function(app){
 		password : 'root',
 		database : 'portal_noticias', //Database usada armazenar/recuperar noticias	
 	});
+
+	//<sql> = Requisição SQL em si
+	//<func callback> = Ação executada com a informação recuperada
+	//connection.query(<sql>, <func callback>);
+	connection.query('select * from noticias', function(error, result){
+		res.send(result);
+	});
+	
 	app.get('/noticias', function(req, res){
 		res.render("noticias/noticias");
 	});

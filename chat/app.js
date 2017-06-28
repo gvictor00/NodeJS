729d2bbaa2208713e54748d9622be650ea0725e4
George <gvictor00@gmail.com>
2017-06-28 11:48:02 -0300

    Configuração do Servidor Multiroom Chat

---
+++ chat/app.js
@@ -0,0 +1,8 @@
+/* importar configurações do servidor */
+
+var app = require('./config/server');
+
+/* parametrizar porta de escuta*/
+app.listen(80, function(){
+	console.log('Servidor online');
+}

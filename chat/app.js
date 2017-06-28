8463e6c85c77e44328ab16bdef8497466826e7d1
George <gvictor00@gmail.com>
2017-06-28 18:55:45 -0300

    Criação do websocket

--- chat/app.js
+++ chat/app.js
@@ -8,4 +8,14 @@ var server = app.listen(3000, function(){
 });
 
 /* define o ponto de escuta da porta 3000 */
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+/* criar a conexão por websocket e escuta eventos de conexão*/
+
+io.on('connection', function(socket){
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+});

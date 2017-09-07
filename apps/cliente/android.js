7724da58d6c64732302999226e763648271394a3
George <gvictor00@gmail.com>
2017-09-07 15:51:33 -0300

    Aula #2, tratatamento de requisições JSON

--- apps/cliente/android.js
+++ apps/cliente/android.js
@@ -4,17 +4,26 @@ var opcoes = {
 	hostname: 'localhost',
 	port: '80',
 	path: '/',
+	method: 'post',
 	headers: {
 		// Envia o que o cliente espera receber
 		//'Accept' : 'text/html'
-		'Accept' : 'application/json'
+		'Accept' : 'application/json',
+		//'Content-type' : 'application/x-www-form-urlencoded'
+		'Content-type' : 'application/json'
+
 	}
 }
 
+//Content-type
+var html = 'nome=José'; //x-www-form-urlencoded
+var json = {nome : 'José'};
+var string_json = JSON.stringify(json);
+
 var buffer_corpo_response = [];
 
-// Permite requisição via GET
-http.get(opcoes,function(res){
+// Permite requisição via GET (Request)
+var req = http.request(opcoes,function(res){
 	// ON = while loading
 	res.on('data', function(chunk){
 		buffer_corpo_response.push(chunk);
@@ -32,3 +41,9 @@ http.get(opcoes,function(res){
 
 	});
 });
+
+//Anexa string no request
+//req.write(html);
+req.write(string_json);
+//Envia a instância do objeto
+req.end();
--- apps/servidor/app/routes/index.js
+++ apps/servidor/app/routes/index.js
@@ -14,4 +14,9 @@ module.exports = function(application){
 			}
 		});
 	});
+
+	application.post('/', function(req, res){
+		var dados = req.body;
+		res.send(dados);
+	});
 }
